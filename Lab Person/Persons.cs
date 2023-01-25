4b425af0912c11b860836590f23734be45d53d5d
Jonathon Kelby Lyng <118222543+kelbylyng@users.noreply.github.com>
2023-01-24 22:49:45 -0700

    Add files via upload

---
+++ Lab Person/Persons.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Person
+{
+    /// <summary>
+    /// Jonathon Lyng
+    /// Jan 19,2023
+    /// </summary>
+    internal class Persons
+    {
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string faveColor;
+        private int age;
+        private bool isWorking;
+
+        public string FirstName
+        { get
+            { return this.firstName; }
+
+        }
+        public string LastName
+        {
+            get
+            { return this.lastName; }
+        }
+        public string FaveColor
+        { get
+            { return this.faveColor; }
+        }
+
+        public int Age
+        { get
+            { return this.age; } 
+        }
+
+        public Persons(int personId, string firstName, string lastName, string faveColor, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.faveColor = faveColor;
+            this.isWorking = isWorking;
+        }
+
+
+        //This displays the persons info in a sentence
+        public void DisplayInfo()
+        {
+            int personId = this.personId;
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            int age = this.age;
+            string faveColor = this.faveColor;
+            bool isWorking = this.isWorking;
+
+            Console.WriteLine(personId + ": " + firstName + "'s " + lastName + " favorite colour is " + faveColor);
+        }
+
+        //This changes the faveorite color of the person
+        public void ChangeFaveColor(string faveColor)
+        {
+            this.faveColor = faveColor;
+        }
+
+        //This adds ten years to the persons current age
+        public string GetAgeInTenYears()
+        {
+            int addTenYears = age + 10;
+            string newAge = firstName + " " + lastName + "'s age in 10 years will be " + addTenYears;
+            return newAge;
+        }
+
+        //This shows the person selected's info into a list
+        public override string ToString()
+        {
+            string dataList = string.Empty;
+
+            dataList += "Person Id: " + this.personId + "\n";
+            dataList += "First Name: " + this.firstName + "\n";
+            dataList += "Last Name: " + this.lastName + "\n";
+            dataList += "Favorite Color: " + this.faveColor + "\n";
+            dataList += "Age: " + this.age + "\n";
+            dataList += "Is Working: " + this.isWorking + "\n";
+
+            return dataList;
+        }
+
+    }
+}
---
+++ Lab Person/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a
+            Persons person1;
+            person1 = new Persons(1, "Ian", "Brooks", "Red", 30, true);
+            Persons person2;
+            person2 = new Persons(2, "Gina", "James", "Green", 18, false);
+            Persons person3;
+            person3 = new Persons(3, "Mike", "Briscoe", "Blue", 45, true);
+            Persons persons4;
+            persons4 = new Persons(4, "Mary", "Beals", "Yellow", 28, true);
+            Console.WriteLine("This is: " + person1);
+
+            //b
+            person2.DisplayInfo();
+
+            //c
+            Console.WriteLine(person3.ToString());
+
+            //d 
+            person1.ChangeFaveColor("White");
+            person1.DisplayInfo();
+
+            //e
+            Console.WriteLine(persons4.GetAgeInTenYears());
+
+
+        }
+    }
+}
